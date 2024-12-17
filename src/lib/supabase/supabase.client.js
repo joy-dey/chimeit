745191a46a85919759b47d2977f5f10cf586e47b
joydey <joy.dey@xlayer.in>
2024-12-17 18:33:44 +0530

    feat(config): add Supabase configuration in supabase.client.js

---
+++ src/lib/supabase/supabase.client.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+import { SUPABASE_URL,SUPABASE_KEY } from "$env/static/private";
+
+const supabaseUrl = SUPABASE_URL;
+const supabaseKey = SUPABASE_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
